5681747202ae4e0559160ed1010728cc7b36f7a6
Joe J Gonzales V <joegv7@gmail.com>
2017-10-30 13:56:47 +0100

    Creando un Hola Mundo en Kotlin.

--- Ejercicios-Basicos/holamundo.kt
+++ Ejercicios-Basicos/holamundo.kt
@@ -1,6 +1,13 @@
-//fun es la funcion sería equivalente a public static void MAIN,
+//fun es la funcion sería equivalente a public static void MAIN o public class,
 // de parámetro le asignamos Array y el tipo string
+//equivalencia: public static void main(String [ ] args)
+//Recordamos que en kotlin no hacen falta punto y coma ";".
 
 fun main(parametro: Array<String>) {
-    print("Hola Mundo Cruel")
+    print("Hola Mundo Cruel") //Simplemente con un Print mostramos en linea pero no hace salto
+    print("Hola Mundo no Salta Linea")
+    
+    println("Hola Mundo Dos") //Con el Println saltamos Linea.
+    println("Hola Mundo Saltamos Linea")
+    
 }
